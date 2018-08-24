71966345419c72c4ba2553a7a827ba6f9e9d0ec0
kimdhendrick <kimdhendrick@gmail.com>
2018-08-23 19:21:11 -0600

    Extract LINE_SEPARATOR

--- src/Bottles.java
+++ src/Bottles.java
@@ -1,44 +1,42 @@
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.IntStream.rangeClosed;
 
 public class Bottles {
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+
     public String song() {
         return verses(99, 0);
     }
 
     public String verses(int starting, int ending) {
-        List<String> strings = IntStream
-                .range(ending, starting + 1)
-                .map(i -> starting - i + ending)
-                .mapToObj(i -> verse(i))
-                .collect(Collectors.toList());
-
-        return String.join("\n", strings);
+        return rangeClosed(ending, starting)
+                .map(i -> (ending + starting) - i)
+                .mapToObj(bottleNumber -> verse(bottleNumber))
+                .collect(joining(LINE_SEPARATOR));
     }
 
     public String verse(int number) {
         switch (number) {
             case 0:
                 return "No more bottles of beer on the wall, " +
-                        "no more bottles of beer.\n" +
+                        "no more bottles of beer." + LINE_SEPARATOR +
                         "Go to the store and buy some more, " +
-                        "99 bottles of beer on the wall.\n";
+                        "99 bottles of beer on the wall." + LINE_SEPARATOR;
             case 1:
                 return "1 bottle of beer on the wall, " +
-                        "1 bottle of beer.\n" +
+                        "1 bottle of beer." + LINE_SEPARATOR +
                         "Take it down and pass it around, " +
-                        "no more bottles of beer on the wall.\n";
+                        "no more bottles of beer on the wall." + LINE_SEPARATOR;
             case 2:
                 return "2 bottles of beer on the wall, " +
-                        "2 bottles of beer.\n" +
+                        "2 bottles of beer." + LINE_SEPARATOR +
                         "Take one down and pass it around, " +
-                        "1 bottle of beer on the wall.\n";
+                        "1 bottle of beer on the wall." + LINE_SEPARATOR;
             default:
-                return String.format("%d bottles of beer on the wall, ", number) +
-                        String.format("%d bottles of beer.\n", number) +
+                return number + " bottles of beer on the wall, " +
+                        number + " bottles of beer." + LINE_SEPARATOR +
                         "Take one down and pass it around, " +
-                        String.format("%d bottles of beer on the wall.\n", number - 1);
+                        (number - 1) + " bottles of beer on the wall." + LINE_SEPARATOR;
         }
     }
 }

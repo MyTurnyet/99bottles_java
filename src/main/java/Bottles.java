4b52d0b00871d59603fa617564d28d1c02bddb61
Janeice <Janeice@192.168.1.251>
2018-09-16 19:36:33 -0400

    Listing 4.10: Send Pronoun In Else Branch

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -30,7 +30,7 @@ public class Bottles {
             default:
                 return number + " " + container(number) + " of beer on the wall, " +
                         number + " " + container(number) + " of beer." + LINE_SEPARATOR +
-                        "Take one down and pass it around, " +
+                        "Take " + pronoun() + " down and pass it around, " +
                         (number - 1) + " " + container(number - 1) + " of beer on the wall." + LINE_SEPARATOR;
         }
     }

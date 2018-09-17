0f242e58ab01fa277c593287322ca7bd6099ab86
Janeice <Janeice@192.168.1.251>
2018-09-16 21:20:24 -0400

    Listing 4.26: 1 Case Passes An Argument

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -26,7 +26,7 @@ public class Bottles {
                 return number + " " + container(number) + " of beer on the wall, " +
                         number + " " + container(number) + " of beer." + LINE_SEPARATOR +
                         "Take " + pronoun(number) + " down and pass it around, " +
-                        quantity() + " bottles of beer on the wall." + LINE_SEPARATOR;
+                        quantity(number - 1) + " bottles of beer on the wall." + LINE_SEPARATOR;
             default:
                 return number + " " + container(number) + " of beer on the wall, " +
                         number + " " + container(number) + " of beer." + LINE_SEPARATOR +
@@ -57,7 +57,7 @@ public class Bottles {
     
     private String quantity(int number) {
         if(number == 0){
-            return "No more";
+            return "no more";
         }
         return String.valueOf(number);
     }

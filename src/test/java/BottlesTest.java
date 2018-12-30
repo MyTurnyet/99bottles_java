4951e335989b3f32bfef621f6a604edcad4ac4da
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 18:38:10 -0500

    Listing 2.1: Verse 99 Test

---
+++ src/test/java/BottlesTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BottlesTest {
+
+    private static final String LINE_BREAK = System.getProperty("line.separator");
+    
+    private Bottles bottles;
+    
+    @Before
+    public void setUp() throws Exception {
+        bottles = new Bottles();
+    }
+
+    @Test
+    public void verse_99() throws Exception {
+        String actual = bottles.verse(99);
+        assertEquals(
+                "99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK, 
+                actual);
+    }
+}

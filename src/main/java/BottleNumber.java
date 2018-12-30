121b5130a489ec0dde370a50fbaeae4570b870e2
Janeice <Janeice@192.168.1.169>
2018-12-29 21:57:23 -0500

    Listing 5.9: Obsessive Methods Copied To Bottle Number

--- src/main/java/BottleNumber.java
+++ src/main/java/BottleNumber.java
@@ -1,4 +1,42 @@
 
 public class BottleNumber {
 
+    
+    String container(int number) {
+        if (number == 1) {
+            return "bottle";
+        }
+
+        return "bottles";
+    }
+
+    String pronoun(int number) {
+        if (number == 1) {
+            return "it";
+        }
+
+        return "one";
+    }
+
+    String quantity(int number) {
+        if (number == 0) {
+            return "no more";
+        }
+        return String.valueOf(number);
+    }
+
+    String action(int number) {
+        if (number == 0) {
+            return "Go to the store and buy some more";
+        }
+
+        return "Take " + pronoun(number) + " down and pass it around";
+    }
+
+    int successor(int number) {
+        if (number == 0) {
+            return 99;
+        }
+        return number - 1;
+    }
 }

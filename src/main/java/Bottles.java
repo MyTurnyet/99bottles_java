554852848ac1bde6fa9947374a2ed9d7535a4608
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 19:36:50 -0500

    Listing 2.5: Sparse Conditional

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -4,13 +4,16 @@ public class Bottles {
     private static final String LINE_BREAK = System.getProperty("line.separator");
 
     String verse(int number) {
-        if (number == 99) {
-            return "99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
-                    + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK;
-        } else {
-            return "3 bottles of beer on the wall, 3 bottles of beer." + LINE_BREAK
-                    + "Take one down and pass it around, 2 bottles of beer on the wall." + LINE_BREAK;
+        int i;
 
+        if(number == 99) {
+            i = 99;
+        }else {
+            i = 3;
         }
+        
+        return i + " bottles of beer on the wall, " + i + " bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall." + LINE_BREAK;
+
     }
 }

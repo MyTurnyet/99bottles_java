140bc9fed630e4b662fb722ec943309e11e3e500
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 20:11:58 -0500

    Listing 2.10: Verse 1 Test

--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class BottlesTest {
 
     private static final String LINE_BREAK = System.getProperty("line.separator");
-    
+
     private Bottles bottles;
-    
+
     @Before
     public void setUp() throws Exception {
         bottles = new Bottles();
@@ -17,26 +17,28 @@ public class BottlesTest {
     @Test
     public void verse_99() throws Exception {
         String actual = bottles.verse(99);
-        assertEquals(
-                "99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
-                + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK, 
-                actual);
+        assertEquals("99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK, actual);
     }
-    
+
     @Test
     public void verse_3() throws Exception {
         String actual = bottles.verse(3);
-        assertEquals(
-                "3 bottles of beer on the wall, 3 bottles of beer." + LINE_BREAK
-                + "Take one down and pass it around, 2 bottles of beer on the wall." + LINE_BREAK, 
-                actual);
+        assertEquals("3 bottles of beer on the wall, 3 bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, 2 bottles of beer on the wall." + LINE_BREAK, actual);
     }
-    
+
     @Test
     public void verse_2() throws Exception {
         String actual = bottles.verse(2);
-        assertEquals(
-                "2 bottles of beer on the wall, 2 bottles of beer." + LINE_BREAK
-                + "Take one down and pass it around, 1 bottle of beer on the wall." + LINE_BREAK, 
-                actual);    }
+        assertEquals("2 bottles of beer on the wall, 2 bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, 1 bottle of beer on the wall." + LINE_BREAK, actual);
+    }
+    
+    @Test
+    public void verse_1() throws Exception {
+        String actual = bottles.verse(1);
+        assertEquals("1 bottle of beer on the wall, 1 bottle of beer." + LINE_BREAK
+                + "Take it down and pass it around, no more bottles of beer on the wall." + LINE_BREAK, actual);
+    }
 }

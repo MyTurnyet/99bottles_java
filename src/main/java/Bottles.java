f269621e948661f2b2868554a2c4e5baabe69930
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 19:18:32 -0500

    Listing 2.2: Verse 99 Code

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -1,8 +1,11 @@
 
 public class Bottles {
 
+    private static final String LINE_BREAK = System.getProperty("line.separator");
+
     String verse(int i) {
 
-        return null;
+        return "99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK;
     }
 }

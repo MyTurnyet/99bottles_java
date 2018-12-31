1d1fb5283322f6faae5d050307d1d5aef56772c0
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 19:55:02 -0500

    Listing 2.8: Stark Conditional

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -5,6 +5,11 @@ public class Bottles {
 
     String verse(int number) {
         
+        if(number == 2) {
+            return "2 bottles of beer on the wall, 2 bottles of beer." + LINE_BREAK
+                    + "Take one down and pass it around, 1 bottle of beer on the wall." + LINE_BREAK;
+           
+        }        
         return number + " bottles of beer on the wall, " + number + " bottles of beer." + LINE_BREAK
                 + "Take one down and pass it around, " + (number - 1) + " bottles of beer on the wall." + LINE_BREAK;
 

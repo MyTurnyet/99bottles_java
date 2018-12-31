fffc7a1975d4b32008d0dffb6de378b35482ea8d
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 19:27:10 -0500

    Listing 2.4: Conditional

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -3,9 +3,14 @@ public class Bottles {
 
     private static final String LINE_BREAK = System.getProperty("line.separator");
 
-    String verse(int i) {
+    String verse(int number) {
+        if (number == 99) {
+            return "99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
+                    + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK;
+        } else {
+            return "3 bottles of beer on the wall, 3 bottles of beer." + LINE_BREAK
+                    + "Take one down and pass it around, 2 bottles of beer on the wall." + LINE_BREAK;
 
-        return "99 bottles of beer on the wall, 99 bottles of beer." + LINE_BREAK
-                + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK;
+        }
     }
 }

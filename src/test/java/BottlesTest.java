082d47c12d418a697d30a7c83d612379c7f53168
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 19:23:06 -0500

    Listing 2.3: Verse 3 Test

--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -22,4 +22,13 @@ public class BottlesTest {
                 + "Take one down and pass it around, 98 bottles of beer on the wall." + LINE_BREAK, 
                 actual);
     }
+    
+    @Test
+    public void verse_3() throws Exception {
+        String actual = bottles.verse(3);
+        assertEquals(
+                "3 bottles of beer on the wall, 3 bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, 2 bottles of beer on the wall." + LINE_BREAK, 
+                actual);
+    }
 }

ac1f6298a2f5c73c53471f447484374d9edff02f
Janeice DelVecchio <janeice@gmail.com>
2018-12-30 19:43:39 -0500

    Listing 2.6: Interpolation

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -4,16 +4,9 @@ public class Bottles {
     private static final String LINE_BREAK = System.getProperty("line.separator");
 
     String verse(int number) {
-        int i;
-
-        if(number == 99) {
-            i = 99;
-        }else {
-            i = 3;
-        }
         
-        return i + " bottles of beer on the wall, " + i + " bottles of beer." + LINE_BREAK
-                + "Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall." + LINE_BREAK;
+        return number + " bottles of beer on the wall, " + number + " bottles of beer." + LINE_BREAK
+                + "Take one down and pass it around, " + (number - 1) + " bottles of beer on the wall." + LINE_BREAK;
 
     }
 }
